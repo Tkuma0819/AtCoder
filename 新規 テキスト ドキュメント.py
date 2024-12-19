fed71d9cf62411c0c9626c9482c4bb31d7a3a026
Tkuma0819 <22411253th@tama.ac.jp>
2024-12-19 17:38:56 +0900

    a

---
+++ 新規 テキスト ドキュメント.py
@@ -0,0 +1,43 @@
+"""
+# 文字列型
+s = input()
+
+# 整数型
+n = int(input())
+
+# 変数 s,n の値を出力
+print(s)
+print(n + 1)
+"""
+"""
+g = input()
+print()
+# 回答例 上と下同じ出力をする
+g1 = input()
+print(g1 + g1)
+"""
+"""
+h = int(input())
+# 回答例
+n = int(input())
+print(n+1)
+# 回答例
+n = int(input())
+n = n + 1
+# 「n += 1」も、上と同義
+print(n)
+# 回答例
+print(int(input()) + 1)
+"""
+"""
+m = list(map(int,input().split()))
+print(m)
+for i in m:
+    print(i)
+"""
+f = map(input().split())
+print(f)
+"""
+n = map(int,input().split())
+print(list(n))
+"""
