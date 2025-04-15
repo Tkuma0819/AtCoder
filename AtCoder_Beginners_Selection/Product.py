ff75a864591e5a2e8f8d061bf4a6192b3db1d1cc
Tkuma0819 <22411253th@tama.ac.jp>
2025-04-16 01:47:12 +0900

    Add files via upload

---
+++ AtCoder_Beginners_Selection/Product.py
@@ -0,0 +1,21 @@
+a,b = map(int,input().split())
+ab = a * b
+if ab % 2 == 1:
+    print('Odd')
+else:
+    print('Even')
+'''
+a, b = map(int,input().split())
+print(a)
+print(b)
+ab = a * b
+print(ab)
+c = str(ab)
+d = list(c)
+print(d)
+print(d[-1])
+if d[1] == ['1','3','5','7','9']:
+    print('Odd')
+else:
+    print('Even')
+'''
